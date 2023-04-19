a3dbcc87b30adf1a7383c7c91c5d2ac4592e0001
Daniel <madureiradaniel600@gmail.com.br>
2023-04-19 16:02:30 -0300

    Iniciando Api, criando carteira

---
+++ routes/CaixaEletronico.js
@@ -0,0 +1,19 @@
+// Depositar/comprar moedinhas | dinheiro(câmbio) -> Troca de real para Moedinha..
+// você 1 real -> 1 moedinha
+
+// sacar - queimar os tokens e transforma em saldo R$
+
+// transferir para outra conta
+
+// Pagar um boleto
+// Pessoa informa o valor do boleto e um número qualquer (Simular código de barras).
+// Dá como o pago o boleto e transferir da carteira da pessoa que está pagando para a carteira do banco.
+// 
+
+// verificar o saldo
+
+// extrato das transações
+
+// configurar na sua máquina
+// Banco de dados
+// PostgresDb
